2573571258cbe4348ecb7c4643fc60c2061f6b6c
Gpioblink <gpioblink@aol.com>
2019-02-26 21:57:26 +0900

    ArrayのcallBack実験

--- node1/cap3/ex.js
+++ node1/cap3/ex.js
@@ -1,27 +1,97 @@
-var ary1 = ['Sato', 'Takae', 'Osada', 'Hio', 'Saitoh', 'Sato'];
-var ary2 = ['Yabuki', 'Aoki', 'Moriyama', 'Yamada'];
+/* 
 
-console.log(ary1.length);
-console.log(Array.isArray(ary1));
-console.log(ary1.toString()); //全てが「,」で連結されて出力
-console.log(ary1.indexOf('Sato'));
-console.log(ary1.lastIndexOf('Sato'));
+配列の内容を順に処理する
+forEachメソッド
 
-console.log(ary1.concat(ary2)); //JS上でみたままのような形式で出力
-console.log(ary1.join('/')); // "/"で連結して出力
-console.log(ary1.slice(1)); //１番めから最後の要素までを出力
-console.log(ary1.slice(1,2)); //1番目から2番目までの要素を出力
-console.log(ary1.splice(1,2,"Kakeya","Yamaguchi")); //1から2番目までの要素を置き換え。戻値は置き換え対称の置換え前の要素を取得。置き換え前と置き換え後の数は必ずしも一致している必要はない。
-console.log(ary1); //上で置き換え後の配列を取得できる
-console.log(Array.of(20,40,60)); //可変長引数を配列にする静的メソッド
-console.log(ary1.copyWithin(1,3,5)); // 4〜5番目の要素を２〜３番めの位置にコピー。戻値はコピー後の配列
-console.log(ary1); //上でコピー後の配列を取得できる
+array.forEach(callback [,that])
+    array: 配列オブジェクト
+    callback: 個々の要素を処理するための関数
+    that: 関数callbackの中でthisが示すオブジェクト
 
-console.log(ary2.fill('Suziki', 1, 3)); //2〜3番目の要素をSuzukiで置き換え
+*/
 
-console.log(ary1.pop());
-console.log(ary1.push("Kondo"));
-console.log(ary1.shift());//先頭を削除するバージョンのpop
-console.log(ary1.unshift("Ozawa","Kuge")); //先頭を追加するバージョンのpush
-console.log(ary1.reverse());
-console.log(ary1.sort());
+var data = [2, 3, 4, 5];
+data.forEach(function(value, index, array){
+    console.log(value*value);
+});
+
+/* 
+
+配列を指定されたルールで加工するmapメソッド
+forEachとの違いは加工けったを戻り値として返すこと
+mapメソッド
+
+array.map(callback [,that])
+    array: 配列オブジェクト
+    callback: 個々の要素を加工するための関数
+    that: 関数callbackの中でthisが示すオブジェクト
+
+*/
+
+var result = data.map(function(value, index, array){
+    return value*value;
+});
+
+/* 
+
+someメソッド
+要素が条件に合致したかをtrue/falseで返し
+1つでも条件に合致する要素がればtrueを返す
+
+ちなみにeveryメソッドはその逆を行う
+
+array.some(callback [,that])
+    array: 配列オブジェクト
+    callback: 個々の要素を判定するための関数
+    that: 関数callbackの中でthisが示すオブジェクト
+
+*/
+
+var data = [4, 9, 16, 25];
+var result = data.some(function(value, index, array){
+    return value % 3 === 0;
+});
+
+if (result) {
+    console.log('3の倍数が見つかりました！');
+} else {
+    console.log('3の倍数は見つかりませんでした');
+}
+
+/*
+
+またsome/everyの書き方で、配列の内容をfilterすることができる。
+以下を実行するとresultに9と25が残る
+
+*/
+
+var data = [4, 9, 16, 25];
+var result = data.filter(function(value, index, array){
+    return value % 2 === 1;
+});
+
+//sortのルールを変更するには、引数が２つの負数と整数を返す関数を作ればいい
+
+//これだと辞書順のソート
+var ary = [5, 25, 10];
+console.log(ary.sort());
+
+//これで数値順のソート
+console.log(ary.sort(function(x,y){
+    return x-y;
+}));
+
+//キー順のソート
+var classes = ['部長', '課長', '主任', '担当'];
+var members = [
+    {name: "あい うえお", clazz: '主任'},
+    {name: "かき くけこ", clazz: '部長'},
+    {name: "さし すせそ", clazz: '担当'},
+    {name: "たち つてと", clazz: '課長'},
+    {name: "なに ぬねの", clazz: '担当'},
+];
+console.log(classes.sort(function(x,y){
+    return classes.indexOf(x.clazz) - classes.indexOf(y.clazz);
+}));
+
+
