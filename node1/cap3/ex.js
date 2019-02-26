dc3a3a428393018c5171f0de5654da9e3089e1f0
Gpioblink <gpioblink@aol.com>
2019-02-26 17:39:59 +0900

    いろいろ計算するMathオブジェクトの実験

--- node1/cap3/ex.js
+++ node1/cap3/ex.js
@@ -1,6 +1,25 @@
-let sym1 = Symbol('sym');
-let sym2 = Symbol('sym');
-
-console.log(typeof sym1);
-console.log(sym1.toString());
-console.log(sym1 === sym2);
+console.log(Math.abs(-100));
+console.log(Math.clz32(1));
+console.log(Math.min(20,40,60));
+console.log(Math.max(20,40,60));
+console.log(Math.pow(5,3));
+console.log(Math.random()); //0から1の間
+console.log(Math.sign(-100)); //正・負・０いずれかの符号
+console.log(Math.ceil(1234.56));
+console.log(Math.ceil(-1234.56));
+console.log(Math.floor(1234.56));
+console.log(Math.floor(-1234.56));
+console.log(Math.round(1234.56));
+console.log(Math.round(-1234.56));
+console.log(Math.tranc(1234.56));
+console.log(Math.tranc(-1234.56));
+console.log(Math.sqrt(81));
+console.log(Math.cbrt(81));
+console.log(Math.hypot(3,4)); //sqrt(3*3 + 4*4)的なやつ
+console.log(Math.cos(1));
+console.log(Math.sin(1));
+console.log(Math.tan(1));
+console.log(Math.atan2(1,3)); //2変数のアークタンジェント
+console.log(Math.log(10));
+console.log(Math.exp(3)); // 指数関数
+console.log(Math.expm1(1)); //e^1 -1
