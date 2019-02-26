e81f76db946e91b22f9259e68f7cae8085b4bf45
Gpioblink <gpioblink@aol.com>
2019-02-26 18:26:17 +0900

    spliceについての解説を追加

--- node1/cap3/ex.js
+++ node1/cap3/ex.js
@@ -11,7 +11,7 @@ console.log(ary1.concat(ary2)); //JS上でみたままのような形式で出�
 console.log(ary1.join('/')); // "/"で連結して出力
 console.log(ary1.slice(1)); //１番めから最後の要素までを出力
 console.log(ary1.slice(1,2)); //1番目から2番目までの要素を出力
-console.log(ary1.splice(1,2,"Kakeya","Yamaguchi")); //1から2番目までの要素を置き換え。戻値は置き換え対称の置換え前の要素を取得
+console.log(ary1.splice(1,2,"Kakeya","Yamaguchi")); //1から2番目までの要素を置き換え。戻値は置き換え対称の置換え前の要素を取得。置き換え前と置き換え後の数は必ずしも一致している必要はない。
 console.log(ary1); //上で置き換え後の配列を取得できる
 console.log(Array.of(20,40,60)); //可変長引数を配列にする静的メソッド
 console.log(ary1.copyWithin(1,3,5)); // 4〜5番目の要素を２〜３番めの位置にコピー。戻値はコピー後の配列
