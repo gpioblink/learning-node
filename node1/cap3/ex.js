8cdc8b6e9521b77565dd0aefb090601f3ea5899a
Gpioblink <gpioblink@aol.com>
2019-02-26 17:00:01 +0900

    数値形式を変換するtoXxxxxメソッドの実験

---
+++ node1/cap3/ex.js
@@ -0,0 +1,10 @@
+var num1 = 255;
+console.log(num1.toString(16));
+console.log(num1.toString(8));
+
+var num2 = 123.45678;
+console.log(num2.toExponential(2));
+console.log(num2.toFixed(3));
+console.log(num2.toFixed(7));
+console.log(num2.toPrecision(10));
+console.log(num3.toPrecision(6));
