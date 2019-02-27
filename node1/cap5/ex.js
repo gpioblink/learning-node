a74ce81f562b856f79831370e6847a920a09c194
Gpioblink <gpioblink@aol.com>
2019-02-28 05:06:11 +0900

    自作関数へのイテレータ実装

--- node1/cap5/ex.js
+++ node1/cap5/ex.js
@@ -1,41 +1,25 @@
-//Object.definePropertyの実験
+//自作クラスへのイテレータの実装
 
-function Triangle(){
-    var _base;
-    var _height;
-
-    Object.defineProperties(this, {
-        'height': {
-            get: function() {
-                return _height;
-            },
-
-            set: function(height) {
-                if(typeof height === 'number' && height > 0){
-                    _height = height;
-                }
-            }
-        },
-        'base': {
-            get: function() {
-                return _base;
-            },
-
-            set: function(base) {
-                if(typeof base === 'base' && base > 0){
-                    _base = base;
-                }
-            }
-        }
-    });
-
-    Triangle.prototype.getArea = function() {
-        return this.base * this.height / 2:
+class MyIterator{
+    constructor(data){
+        this.data = data;
     }
 
-    var t = new Triangle();
-    t.base = 10;
-    t.height = 2;
-    console.log(t.base);
-    console.log(t.height);
+    //これを実装すると自作関数でもイテレータが使える！
+    [Symbol.iterator](){
+        let current = 0;
+        let that = this;
+        return {
+            next(){
+                return current < this.data.length ?
+                {value: that.data[current++], done: false}:
+                {done: true};
+            }
+        };
+    }
+}
+
+let itr = new MyIterator(['one','two','three']);
+for(let value of itr){
+    console.log(value);
 }
