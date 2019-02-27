cfcb54695e07f693b634db7ea9332162ed77bd98
Gpioblink <gpioblink@aol.com>
2019-02-28 02:59:05 +0900

    プロトタイプオブジェクト指向のプライベートメンバ

--- node1/cap5/ex.js
+++ node1/cap5/ex.js
@@ -1,20 +1,35 @@
-//継承の練習
+//プライベートメンバ
 
-var Animal = function(){};
+function Triangle(){
+    var _base; //thisのかわりにvarで作ってセッターゲッターからアクセス
+    var _height;
 
-Animal.prototype = {
-    walk: function(){
-        console.log('とことこ');
+    var _checkArgs = function(val) {
+        return (typeof val === 'number' && val>0);
     }
-};
 
-var Dog = function(){
-    Animal.call(this);
-};
-Dog.prototype = new Animal(); //DogオブジェクトのインスタンスからAnimalオブジェクトで定義されたwalkメソッドを呼び出されるようになる
-Dog.prototype.bark = function(){
-    console.log('わんわん');
+    this.setBase = function(base){
+        if(_checkArgs(base)) {_base = base;}
+    }
+    this.getBase = function(){return _base}
+
+    this.setHeight = function(height){
+        if(_checkArgs(height)){_height = height;}
+    }
+    this.getHeight = function(){return _height;}
 }
-var d = new Dog();
-d.walk();
-d.bark();
+
+Triangle.prototype.getArea = function(){
+    return this.getBase() * this.getHeight() /2;
+}
+
+var t = new Triangle();
+t._base = 10;
+t._height = 2;
+console.log(t.getArea()); //NaN
+
+t.setBase(10);
+t.setHeight(2);
+console.log(t.getBase());
+console.log(t.getHeight());
+console.log(t.getArea());
