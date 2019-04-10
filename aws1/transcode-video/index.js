7caf34d0c12fc948155da73e5aeb2545e508d054
Kai Yamada <gpioblink@aol.com>
2019-04-10 22:08:45 +0900

    拡張子判定を改善

--- aws1/transcode-video/index.js
+++ aws1/transcode-video/index.js
@@ -12,9 +12,10 @@ exports.handler = function(event, context, callback){}
  exports.handler = function(event, content, callback){
      var key = event.Records[0].s3.object.key;
      var sourceKey = decodeURIComponent(key.replace(/\+/g," ")); //S3のキー名はURLエンコードされるのででコードが必要
-     var outputKey = sourceKey.split('.')[0]; //拡張子を除去 
+     var outputKey = sourceKey.match(/(.+)(\.[^.]+$)/)[1]; //拡張子を除去、途中にピリオドがあってもいいように修正 
+     var extension = sourceKey.match(/(.+)(\.[^.]+$)/)[2].slice(1);
      console.log('key:', key, sourceKey, outputKey);
-
+     
      var params = {
          PipelineId: '1553851440639-wirur5',
          OutputKeyPrefix: outputKey + '/', //出力キープレフィックスはトランスコード済み動画バケットのファイルに論理的な階層構造を与える
